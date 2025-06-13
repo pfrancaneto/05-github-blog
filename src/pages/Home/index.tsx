0670fbec276a9ebfc26f361d702d7da7b87a23e6
Pedro França <pfranca.neto@gmail.com>
2025-06-13 15:59:08 -0300

    criando pages

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { HomeContainer } from './styles';
+
+const Home = () => {
+  return (
+    <HomeContainer>
+      <div>Home</div>
+    </HomeContainer>
+  );
+};
+
+export default Home;
---
+++ src/pages/Post/index.tsx
@@ -0,0 +1,11 @@
+import { PostContainer } from './styles';
+
+const Post = () => {
+  return (
+    <PostContainer>
+      <div>POst</div>
+    </PostContainer>
+  );
+};
+
+export default Post;
